46c1258cc8d589ce8cb98a9e09897bae4446eecc
Victoria Pham <victoriapham52@gmail.com>
2023-04-01 17:15:22 -0700

    added more test data

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,6 +7,9 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(10, 15, 5, 30)]
+        [InlineData(100, 206, 21, 327)]
+        [InlineData(-11, 6, -52, -57)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -26,6 +29,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(20,10, 10)]//Add test data <-------
+        [InlineData(-46, 63, -109)]
+        [InlineData(-7, 52, -59)]
+        [InlineData(61, 23, 38)]
         public void SubtractTest(int minuend, int subtrahend, int expected)
         {
             //Start Step 5 here:
@@ -41,7 +47,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(5,5,25)]//Add test data <-------
+        [InlineData(5,5,25)]    //Add test data <-------
+        [InlineData(9, 81, 729)]
+        [InlineData(-23, 4, -92)]
+        [InlineData(100, 69, 6900)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -58,7 +67,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(10,5,2)]//Add test data <-------
+        [InlineData(10,5,2)]    //Add test data <-------
+        [InlineData(78, 3, 26)]
+        [InlineData(522, 2, 261)]
+        [InlineData(2, 1, 2)]
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
